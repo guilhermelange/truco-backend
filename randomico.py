da4663da22b4fc950765c793f532235a153a1b9b
murilogoedert <murilogoedert@gmail.com>
2022-11-06 16:16:29 -0300

    Ajustes gerais

--- randomico.py
+++ randomico.py
@@ -10,14 +10,14 @@ class AlgoritmoRandomico(Algoritmo):
 
         if jogadaAdversario != None and jogadaAdversario['type'] == 'TRUCO':
             if random.randint(0,1) == 1:
-                return self.accept(self.popCarta(random.choice(self.hand)))
+                return self.accept()
             else:
                 return self.run()
 
         randomChoice = random.randint(0, 10)
 
         if randomChoice <= 1 and jogadaAdversario == None and self.isTrucoPermited():
-            return self.truco(self.popCarta(random.choice(self.hand)))
+            return self.truco()
         elif randomChoice == 2:
             return self.run()
         else:
